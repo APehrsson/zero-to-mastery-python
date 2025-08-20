6cab961acd40e16ce9d315f9cf024d487f99e7e0
Andreas Pehrsson <apehrsson@hotmail.com>
2025-08-20 11:54:55 +0200

    add more solutions

---
+++ 10-Advanced-Python-Generators/10-168-under-the-hood.py
@@ -0,0 +1,30 @@
+def special_for_loop(iterable):
+    iterator = iter(iterable)
+    while True:
+        try:
+            print(iterator)
+            next(iterator)
+        except StopIteration:
+            break
+
+special_for_loop([1,2,3]) #uses the same space to store on the computer
+
+class MyGen():
+    current = 0
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if MyGen.current < self.last:
+            num = MyGen.current
+            MyGen.current +=1
+            return num
+        raise StopIteration
+
+gen = MyGen(0,100)
+for i in gen:
+    print(i)
